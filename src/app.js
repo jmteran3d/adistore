72022163911897aba2ce2fd5754a5865e0bdac16
jmteran3d <jemate@gmail.com>
2025-07-16 14:54:38 -0400

    2daPreEntregaTeran cambios

--- src/app.js
+++ src/app.js
@@ -3,8 +3,17 @@ import { engine } from "express-handlebars";
 import viewsRouter from "./routes/views.router.js";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import http from "http";
+import { Server } from "socket.io";
+
 
 const app = express();
+const server = http.createServer(app);
+
+//configuramos nuestro server para que acepte solicitudes websockets
+// Input - Output
+const io = new Server(server);
+
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
@@ -13,14 +22,16 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./src/views");
 
+//endpoints
 /* app.get("/", (req, res)=> {
-  res.json({ message: "Hola! Bienvenido a AdiStore" });
+  res.render("home");
 }); */
 
-//endpoints
-app.get("/", (req, res)=> {
-  res.render("home");
-});
+/* app.get("/", async (req, res) => {
+  const products = await ProductManager.getProducts(); // o como estés cargándolos
+  res.render("home", { products });
+}); */
+
 app.get("/contact", (req, res)=> {
   res.render("contact");
 });
@@ -29,6 +40,24 @@ app.use("/", viewsRouter);
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-app.listen(8080, () => {
-  console.log("Servidor corriendo en http://localhost:8080");
+//persistencia en memoria de los mensajes de chat
+const messages = [];
+
+//websockets desde el servidor
+io.on("connection", (socket)=> {
+  //emitimos un evento desdel el servidor al cliente
+  socket.emit("message history", messages);
+
+  console.log("Nuevo usuario conectado");
+
+  socket.on("new message", (data)=> {
+    messages.push(data);
+    
+    //transmitimos el nuevo mensaje a todos los clientes conectados
+    io.emit("broadcast new message", data);
+  });
+});
+
+server.listen(8080, ()=> {
+  console.log("Servidor iniciado correctamente en http://localhost:8080");
 });
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -4,7 +4,7 @@ import ProductManager from "../managers/ProductManager.js";
 const router = Router();
 const productManager = new ProductManager("./src/data/products.json");
 
-const user = { username: "FedericoDev01", isAdmin: true };
+const user = { username: "JMTeran3D_Dev", isAdmin: true };
 
 const middlewareIsAdmin = (req, res, next) => {
   if(user.isAdmin){
@@ -17,8 +17,16 @@ const middlewareIsAdmin = (req, res, next) => {
 router.get("/", async(req, res)=> {
   try {
     const products = await productManager.getProducts();
+
+    // Validación
+    if (!Array.isArray(products) || products.length === 0) {
+      console.error("❌ 'products' no es un array válido o está vacío");
+      return res.render("home", { products: [], user });
+    }
+
     res.render("home", { products, user });
   } catch (error) {
+    console.error("Error al cargar productos:", error);
     res.render("error");
   }
 });
